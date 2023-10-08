c9fe03c61c8630bb5209e8a67dc126a117ecbb5c
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-08 13:57:25 -0300

    added firebase connection const
    in progress

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import { initializeApp } from "firebase/app";
 dotenv.config();
+
+const firebaseConfig = {
+        apiKey: process.env.FIREAPI,
+        authDomain: process.env.FIREDOMAIN,
+        projectId: process.env.FIREID,
+        storageBucket: process.env.FIREBUCKET,
+        messagingSenderId: process.env.FIRESENDER,
+        appId: process.env.FIREAPPID
+    };
+
+const firebaseAPP = initializeApp(firebaseConfig);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { 
     console.log(`Server running on port ${PORT}`);
+    console.log("Firebase OK : " + firebaseAPP.name)    
 })
