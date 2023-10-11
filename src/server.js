5ab2cbc489e93a332d32240fff7d2b1eb6de1fe1
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-10 21:53:48 -0300

    added firebase constraints

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 dotenv.config();
 
 const firebaseConfig = {
@@ -9,14 +11,17 @@ const firebaseConfig = {
         projectId: process.env.FIREID,
         storageBucket: process.env.FIREBUCKET,
         messagingSenderId: process.env.FIRESENDER,
-        appId: process.env.FIREAPPID
+        appId: process.env.FIREAPPID,
+        measurementId: process.env.FIREMEASUREMENT
     };
 
 const firebaseAPP = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseAPP);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { 
     console.log(`Server running on port ${PORT}`);
-    console.log("Firebase OK : " + firebaseAPP.name)    
+    console.log("Firebase OK")
+    console.log("Firebase Auth OK")   
 })
