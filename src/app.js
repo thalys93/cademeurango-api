4288242f9c10e1ac03ea7ebfd715d0630634b3cf
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-10-17 10:30:44 -0300

    fixed styles in index.html
    and now, te api have a view to visualize routes , and other things

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/routes.js");
+const path = require("path");
 const database = require('./database/migrations/index.js');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 routes(app);
 
